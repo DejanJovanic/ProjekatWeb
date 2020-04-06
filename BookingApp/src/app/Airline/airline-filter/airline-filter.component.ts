a954b860a9160ceb5a90d5caa52398b25e72a0bf
DejanJovanic <34839384+DejanJovanic@users.noreply.github.com>
2020-04-06 16:36:55 +0200

    dorada search i filter bara

--- BookingApp/src/app/Airline/airline-filter/airline-filter.component.ts
+++ BookingApp/src/app/Airline/airline-filter/airline-filter.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AirlineFilterComponent implements OnInit {
 
+  isCollapsed = true;
   constructor() { }
 
   ngOnInit(): void {
--- BookingApp/src/app/Airline/airline-search/airline-search.component.ts
+++ BookingApp/src/app/Airline/airline-search/airline-search.component.ts
@@ -1,12 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-airline-search',
   templateUrl: './airline-search.component.html',
-  styleUrls: ['./airline-search.component.css']
+  styleUrls: ['./airline-search.component.css'],
+  animations: [
+    trigger('smoothCollapse', [
+      state('initial', style({
+        height:'0',
+        overflow:'hidden',
+        opacity:'0'
+      })),
+      state('final', style({
+        overflow:'hidden',
+        opacity:'1'
+      })),
+      transition('initial=>final', animate('750ms')),
+      transition('final=>initial', animate('750ms'))
+    ]),
+  ]
 })
 export class AirlineSearchComponent implements OnInit {
 
+  isCollapsed = true;
   constructor() { }
 
   ngOnInit(): void {
